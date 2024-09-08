056c80f5f7dbe7117332ae1a62fca505f0013bb6
Vlad Urziceanu <vladurziceanu@google.com>
2024-09-08 06:46:24 +0000

    Add my initial configuration

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,10 +1,26 @@
 require "nvchad.mappings"
 
--- add yours here
-
 local map = vim.keymap.set
 
+map("n", "<leader><leader>", "/", { desc = "CMD enter search mode" })
+map("n", "<C-space>", "?", { desc = "CMD enter reverse search mode" })
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+map("n", "0", "^") --0 goes to first character
+map("n", "<leader>ss", "<cmd>w!<cr>", { desc = "CMD save file" })
+map("n", "<leader>tn", "<cmd>tabnew<cr>", { desc = "CMD open a new tab" })
+map("n", "<leader>tc", "<cmd>tabclose<cr>", { desc = "CMD close the current tab" })
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+--Move a line of text with ALT+[jk]
+map("n", "<A-j>", "<cmd>m+<cr>")
+map("n", "<A-k>", "<cmd>m-2<cr>")
+map("i", "<A-j>", "<Esc><cmd>m .+1<CR>==gi")
+map("i", "<A-k>", "<Esc><cmd>m .-2<CR>==gi")
+map("v", "<A-j>", "<cmd>m '>+1<CR>gv=gv")
+map("v", "<A-k>", "<cmd>m '>-2<CR>gv=gv")
+
+map("n", "<leader>gd", "<cmd>DiffviewOpen<cr>")
+map("n", "<leader>gh", "<cmd>DiffviewFileHistory<cr>")
+map("n", "<leader>gfh", "<cmd>DiffviewFileHistory %<cr>")
+
+map("n", "<leader>gb", "<cmd>Git blame<cr>")
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -4,15 +4,58 @@ return {
     -- event = 'BufWritePre', -- uncomment for format on save
     opts = require "configs.conform",
   },
-
-  -- These are some examples, uncomment them if you want to see them work!
   {
     "neovim/nvim-lspconfig",
     config = function()
       require "configs.lspconfig"
     end,
   },
-
+  {
+    "vivien/vim-linux-coding-style",
+    lazy = false,
+    config = function()
+      vim.g.linuxsty_patterns = { "/linux/", "/kernel/", "/kernel-6.6/" }
+    end,
+  },
+  {
+    "dhananjaylatkar/cscope_maps.nvim",
+    cmd = "Cs",
+    dependencies = {
+      "folke/which-key.nvim", -- optional [for whichkey hints]
+      "nvim-tree/nvim-web-devicons", -- optional [for devicons in telescope, fzf or mini.pick]
+    },
+    opts = {
+      prefix = "<C-\\>",
+      cscope = {
+        db_build_cmd_args = { "-Rkb" },
+        picker = "telescope",
+      },
+    },
+  },
+  {
+    "kylechui/nvim-surround",
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup {
+        -- Configuration here, or leave empty to use defaults
+      }
+    end,
+  },
+  {
+    "sindrets/diffview.nvim",
+    cmd = { "DiffviewOpen", "DiffviewFileHistory" },
+  },
+  {
+    "tpope/vim-fugitive",
+    event = "User FilePost",
+  },
+  {
+    "lewis6991/gitsigns.nvim",
+    event = "User FilePost",
+    opts = function()
+      return require "configs.gitsigns"
+    end,
+  },
   -- {
   -- 	"nvim-treesitter/nvim-treesitter",
   -- 	opts = {
