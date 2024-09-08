f39a1fd6a46e52be3779c1a718f450dfb8c02cd2
Vlad Urziceanu <vladurziceanu@google.com>
2024-09-08 10:15:09 +0000

    move autocmds to own file

--- lua/options.lua
+++ lua/options.lua
@@ -11,38 +11,3 @@ vim.highlight.cursorline = 'term=bold cterm=bold ctermbg=236'
 o.list = true
 o.listchars ='tab:» ,trail:·'
 o.scrolloff = 5
-
--- Autocmd commands
-local autocmd = vim.api.nvim_create_autocmd
-
--- Persistent Cursor
-autocmd("BufReadPost", {
-  callback = function()
-    local mark = vim.api.nvim_buf_get_mark(0, '"')
-    local lcount = vim.api.nvim_buf_line_count(0)
-    if mark[1] > 0 and mark[1] <= lcount then
-      pcall(vim.api.nvim_win_set_cursor, 0, mark)
-    end
-  end,
-})
-
--- Cursor Line on each window
-autocmd({ "InsertLeave", "WinEnter" }, {
-  callback = function()
-    local ok, cl = pcall(vim.api.nvim_win_get_var, 0, "auto-cursorline")
-    if ok and cl then
-      vim.wo.cursorline = true
-      vim.api.nvim_win_del_var(0, "auto-cursorline")
-    end
-  end,
-})
-
-autocmd({ "InsertEnter", "WinLeave" }, {
-  callback = function()
-    local cl = vim.wo.cursorline
-    if cl then
-      vim.api.nvim_win_set_var(0, "auto-cursorline", cl)
-      vim.wo.cursorline = false
-    end
-  end,
-})
